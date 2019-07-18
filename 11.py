5cf18fcfd773099426efca7f03bf244a0f5d6258
Aleksandr Nikonov <Stape.90@gmail.com>
2019-07-19 00:33:21 +0300

    test

---
+++ 11.py
@@ -0,0 +1,13 @@
+import os
+dir_name = "venv/"
+
+def my():
+    print('1')
+#Запуск бесконечного цикла
+while True:
+    #Цикл просмотра файлов в папке  venv
+    for file in os.listdir(dir_name):
+        #Проверка налияае файла .py
+        if file.endswith(".py"):
+            #Имя файла .py
+            print(os.path.join("/", file))
