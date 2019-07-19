0e434289a4ed680a438d8f7e3846a336867786c4
Aleksandr Nikonov <Stape.90@gmail.com>
2019-07-19 23:31:15 +0300

    Test v.1

--- 11.py
+++ 11.py
@@ -173,20 +173,112 @@ class Ftp_Connection():
     def put_file_old(self, src, dest):
         #Отображение процесса переноса данных на сервер
         def print_transfer_status(block):
+            print("Получили переменную блок 176 строка", block)
             nonlocal bytes_transferred, total_bytes
             bytes_transferred += len(block)
             if total_bytes > 0:
-                percent = round((bytes_transferred / total_bytes) )
+                percent = round((bytes_transferred / total_bytes) * 100, 2 )
+                print("{} / {} bytes ({}%)".format(bytes_transferred, total_bytes, percent))
+            else:
+                print("{} bytes" .format(bytes_transferred))
+            return block
+        if os.path.isfile(src):
+            print("Просмотр файла строка 185" ,os.path.isfile(src))
+            with open(src, 'rb') as fh:
+                if self.show_progress:
+                    total_bytes = os.path.getsize(src)
+                    bytes_transferred = 0
+                    self.conn.storbinary(cmd='STOR {}'.format(dest), fp=fh, callback=print_transfer_status)
+                else:
+                    self.conn.storbinary(cmd='STOR {}'.format(dest), fp=fh)
+        else:
+            print("Local file does not exist: {}".format(src))
 
+        def delete_file(self, path):
+            try:
+                self.conn.delete(path)
+            except ftplib.error_perm as err:
+                print("Error with permissions: {}".format(str(err)))
+            except ftplib.error_reply as err:
+                print("Error: {}".format(str(err)))
 
+        def move_file(self, src, dest):
+            self.conn.rename(src, dest)
 
+            ### Permissions properties ###
 
+        def can_write_to_dir(self, path):
+            return self._permission_check(path, 'c')
+
+        def can_make_subdirectories(self, path):
+            return self._permission_check(path, 'm')
+
+        def can_list_dir(self, path):
+            return self._permission_check(path, 'l')
+
+        def _permission_check(self, path, perm):
+
+            try:
+                return perm in self._permissons[path]
+            except AttributeError:
+                self._permissions = {}  # first time asking for permissions at all
+            except KeyError:
+                pass  # first time asking for permissions for this path
+
+            # MLSD permission values, from http://tools.ietf.org/html/rfc3659.html
+            # ('incoming', {'perm': 'flcdmpe'})
+            # a = For files only.  The APPE (append) command may be applied to the file.
+            # c = For dirs only.  Files may be created in the directory.
+            # d = For all.  The file/dir may be deleted.
+            # e = For dirs.  The user can enter the directory, and CWD should work.
+            # f = For all.  The file/dir may be renamed.
+            # l = For dirs.  List commands can be used.
+            # m = For dirs.  MKD may be used to make subdirs.
+            # p = For dirs.  Objects in the dir (though not necessarily the dir itself) may be deleted.
+            # r = For files.  RETR may be applied (to retrive the file)
+            # w = For files.  STOR may be applied (to write files)
+
+            # mlsd() returns an iterator.  next(...) grabs items off the iterator until it finds one called '.',
+            # then grabs the string of permissions for it.
+            permissions = list(next(obj for obj in self.conn.mlsd(path, ['perm']) if obj[0] == '.')[1]['perm'])
+            self._permissions[path] = permissions
+
+            return perm in self._permissions[path]
+
+        def connect(self):
+            try:
+                self.conn = ftplib.FTP(self.host, self.username, self.password)
+                self.conn.sendcmd("TYPE i")  # switch to binary mode
+
+                # optimize socket params for download task
+                self.conn.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+                # self.conn.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 75) # not an option on Windows
+                # self.conn.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 60) # not an option on Windows
+
+                self.conn.set_debuglevel(self.FTP_DEBUG_LEVEL)
+                self.conn.set_pasv(True)
+            except ftplib.error_perm as err:
+                print("Error: {}".format(str(err)))
+
+        def disconnect(self):
+            self.conn.quit()
+
+        if __name__ == "__main__":
+            path = '/'
+            filename = '100GB.zip'
+
+            logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
+            ftp = Ftp_Connection('"speedtest.tele2.net"', 'anonymous', '')
+            ftp.put_file(path + filename, path + filename)
+            ftp.get_file(path + filename, path + filename)
+            pass
 
 #Запуск бесконечного цикла
-while True:
+"""while True:
     #Цикл просмотра файлов в папке  venv
     for file in os.listdir(dir_name):
         #Проверка налияае файла .py
         if file.endswith(".py"):
             #Имя файла .py
             print(os.path.join("/", file))
+"""
